fd059f495d52643b24b0a7418e7655484a8372f8
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-07-23 19:59:43 +1200

    Made a calculator

--- test.py
+++ test.py
@@ -1,29 +1,32 @@
-print('**********************')
-print("     Calculator")
-print('**********************')
-i = input('Whats your name? ')
-print('Hello ' + i)
+# print('**********************')
+# print("     Calculator")
+# print('**********************')
+# i = input('Whats your name? ')
+# print('Hello ' + i)
+# while True:
+#     print('1 - Add')
+#     print('2 - Subtract')
+#     print('3 - Multiply')
+#     print('4 - Divide')
+#     cal = input('What calculation do you want? ')
+#     a = input('Enter first number: ')
+#     b = input('Enter second number: ')
+#     if cal == '1':
+#         cal = (int(a) + int(b))
+#     elif cal == '2':
+#         cal = (int(a) - int(b))
+#     elif cal == '3':
+#         cal = (int(a) * int(b))
+#     elif cal == '4':
+#         pcal = (int(a) / int(b))
+#     print('**********')
+#     print(i + ', the total is: ' + str(cal))
+#     print('**********')
+#     x = input('Press q to quit, otherwise continue ')
+#     if x.lower() == 'q':
+#         break
+#     else:
+#         print('')
+#         continue
 
-while True:
-    print('1 - Add')
-    print('2 - Subtract')
-    print('3 - Multiply')
-    print('4 - Divide')
-    cal = input('What calculation do you want? ')
-    a = input('Enter first number: ')
-    b = input('Enter second number: ')
-    if cal == 1:
-        print(i + ', the total is: ' + (int(a) / int(b)))
-    elif cal == 2:
-        print(i + ', the total is: ' - (int(a) / int(b)))
-    elif cal == 3:
-        print(i + ', the total is: ' * (int(a) / int(b)))
-    elif cal == 4:
-        print(i + ', the total is: ' + (int(a) / int(b)))
-    print('')
-    x = input('Press q to quit, otherwise continue ')
-    if x.lower() == 'q':
-        break
-    else:
-        print('')
-        continue
+
